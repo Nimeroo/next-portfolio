598022a32e9d69ec76f34e8dc83e5814ff7e8dc1
Nimeroo <girou.joe@gmail.com>
2023-11-30 14:06:31 -0500

    Hero added to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from "./Components/Hero/Hero"
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Hero/>
     </main>
   )
 }

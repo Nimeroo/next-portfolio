9990a1573a1f6a09fbf144b3d232939891ba30f2
Nimeroo <girou.joe@gmail.com>
2023-12-03 22:56:14 -0500

    Skill component added to About component

--- src/app/Components/About/About.jsx
+++ src/app/Components/About/About.jsx
@@ -1,27 +1,31 @@
 import React from "react";
+import Skills from "../Skills/Skills";
 
 const About = () => {
   return (
     <div>
-      <h2>Who am I?</h2>
-      <p>
-        My name is Joel Giroux and I take delight in creating digital tools that
-        people can utilize to simplify their daily lives.
-      </p>
-      <p>
-        Ever since I was a young boy, I have always had an immense interest in
-        technology so it was only natural that I would eventually find myself
-        coding. I started learning HTML, CSS, and Javascript at only 12 when I
-        wanted to create an animation for a class project. From there I
-        continued pursuing different oppurtunities, from a coding internship to
-        boilster my programming skills, to a coding bootcamp that taught me how
-        web development is done on a professional level.
-      </p>
-      <p>
-        As of now I am currently creating and updating projects as well and
-        looking for new job oppurtunities that may come my way.
-      </p>
-      <img></img>
+      <div>
+        <h2>Who am I?</h2>
+        <p>
+          My name is Joel Giroux and I take delight in creating digital tools
+          that people can utilize to simplify their daily lives.
+        </p>
+        <p>
+          Ever since I was a young boy, I have always had an immense interest in
+          technology so it was only natural that I would eventually find myself
+          coding. I started learning HTML, CSS, and Javascript at only 12 when I
+          wanted to create an animation for a class project. From there I
+          continued pursuing different oppurtunities, from a coding internship
+          to boilster my programming skills, to a coding bootcamp that taught me
+          how web development is done on a professional level.
+        </p>
+        <p>
+          As of now I am currently creating and updating projects as well and
+          looking for new job oppurtunities that may come my way.
+        </p>
+        <img></img>
+      </div>
+      <Skills/>
     </div>
   );
 };

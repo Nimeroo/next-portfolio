d5110d1033a01f08e24fb5b1e79bf1dd95c43bfa
Nimeroo <girou.joe@gmail.com>
2023-12-03 19:57:57 -0500

    hero component content modified

--- src/app/Components/Hero/Hero.jsx
+++ src/app/Components/Hero/Hero.jsx
@@ -6,10 +6,9 @@ import Link from 'next/link'
 const Hero = () => {
     return (
         <div>
-            <h1>Hello, I am</h1>
             <h1>Joel Giroux</h1>
             <h3>Creator of things on the net</h3>
-            <p> I am a software engineer who specializes in programming automated tools and digital creations.</p>
+            <p> I am a software engineer who specializes in programming automated tools and developing digital creations.</p>
             <Link href="/Projects">View my projects!</Link>
         </div>
     )

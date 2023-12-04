1f1293a5f32151a131993050434db4ad523b87ba
Nimeroo <girou.joe@gmail.com>
2023-12-03 19:56:51 -0500

    About component added to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from "./Components/Hero/Hero"
+import About from './Components/About/About'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero/>
+      <About/>
     </main>
   )
 }

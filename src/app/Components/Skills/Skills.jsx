5272af39a3bca87859e03c8952ae086bad36c8a7
Nimeroo <girou.joe@gmail.com>
2023-12-03 22:52:46 -0500

    Skills component created

---
+++ src/app/Components/Skills/Skills.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+        <h3>Proficient Technlogies</h3>
+      <ul>
+        <li>Javascript (ES6+)</li>
+        <li>CSS and CSS-in-JS</li>
+        <li>Sass</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>Next.js</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
